47a5245f90a2385a0d80354069e18de4558bed8e
Gabriela Escobar <g.escobar23@uniandes.edu.co>
2024-04-23 08:39:58 -0500

    comment

--- src/app/vivienda/vivienda.ts
+++ src/app/vivienda/vivienda.ts
@@ -14,6 +14,7 @@ export class Vivienda {
     ocupada: boolean;
     coordX:number;
     coordY: number;
+    //faltan enumeraciones y propietario
     constructor(
         id:number,
         nombre: string,
