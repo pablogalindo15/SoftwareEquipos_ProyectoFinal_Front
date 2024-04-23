171566d71f494b2983ae9744b7d7e2d206019bcc
Gabriela Escobar <g.escobar23@uniandes.edu.co>
2024-04-23 08:39:26 -0500

    vivienda.ts

---
+++ src/app/vivienda/vivienda.ts
@@ -0,0 +1,50 @@
+export class Vivienda {
+    id:number;
+    nombre: string;
+    precio: number;
+    descripcion: string;
+    fotos: string;
+    tamano: number;
+    posiblesEstratos: any;
+    estrato:any;
+    restricciones: string;
+    tipo:any;
+    contacto:string;
+    direccion: string;
+    ocupada: boolean;
+    coordX:number;
+    coordY: number;
+    constructor(
+        id:number,
+        nombre: string,
+        precio: number,
+        descripcion: string,
+        fotos: string,
+        tamano: number,
+        posiblesEstratos: any,
+        estrato:any,
+        restricciones: string,
+        tipo:any,
+        contacto:string,
+        direccion: string,
+        ocupada: boolean,
+        coordX:number,
+        coordY: number,
+    ){
+        this.id = id;
+        this.nombre=nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.fotos = fotos;
+        this.tamano = tamano;
+        this.posiblesEstratos=posiblesEstratos;
+        this.estrato = estrato;
+        this.restricciones = restricciones;
+        this.tipo = tipo;
+        this.contacto = contacto;
+        this.direccion = direccion;
+        this.ocupada = ocupada;
+        this.coordX = coordX;
+        this.coordY = coordY;
+    }
+}
