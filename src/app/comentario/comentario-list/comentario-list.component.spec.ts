6daf2884a4eba32dee07a598aa3ba5ea2dc47347
mh783 <m.hernandezr2@uniandes.edu.co>
2024-05-05 00:00:25 -0500

    comentario

--- src/app/comentario/comentario-list/comentario-list.component.spec.ts
+++ src/app/comentario/comentario-list/comentario-list.component.spec.ts
@@ -1,24 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { ComentarioListComponent } from './comentario-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Propietario } from '../../propietario/propietario';
 
-describe('LugarListComponent', () => {
+describe('ViviendaListComponent', () => {
   let component: ComentarioListComponent;
   let fixture: ComponentFixture<ComentarioListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports:[HttpClientModule],
-      declarations: [ComentarioListComponent]
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      declarations: [ ComentarioListComponent ]
     })
     .compileComponents();
-    
+  }));
+ 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ComentarioListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
+ 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
