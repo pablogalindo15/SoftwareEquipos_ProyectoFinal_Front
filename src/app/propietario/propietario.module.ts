7ef6ac1871e5aff6164b608c7af893677583a8a6
Jeronimo <j.francoc2@uniandes.edu.co>
2024-05-06 12:47:20 -0500

    revert Importante

--- src/app/propietario/propietario.module.ts
+++ src/app/propietario/propietario.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PropietarioListComponent } from './propietario-list/propietario-list.component';
+import { PropietarioDetailComponent } from './propietario-detail/propietario-detail.component';
 
 
 
 @NgModule({
   declarations: [
-    PropietarioListComponent
+    PropietarioListComponent,
+    PropietarioDetailComponent
   ],
   imports: [
     CommonModule
