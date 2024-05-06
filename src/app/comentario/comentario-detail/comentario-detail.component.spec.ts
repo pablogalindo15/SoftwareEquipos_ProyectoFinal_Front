3cdc8907269b52d3d2fbfc2a7db43ba3b17d0f2d
mh783 <m.hernandezr2@uniandes.edu.co>
2024-05-05 22:39:52 -0500

    comentario Detail

--- src/app/comentario/comentario-detail/comentario-detail.component.spec.ts
+++ src/app/comentario/comentario-detail/comentario-detail.component.spec.ts
@@ -1,16 +1,22 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { faker } from '@faker-js/faker';
 import { ComentarioDetailComponent } from './comentario-detail.component';
+import { ComentarioDetail } from '../comentario-detail';
+import { Propietario } from  '../../propietario/propietario';
+import { Comentario } from '../comentario';
 
 describe('ComentarioDetailComponent', () => {
   let component: ComentarioDetailComponent;
   let fixture: ComponentFixture<ComentarioDetailComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule],
       declarations: [ ComentarioDetailComponent ]
     })
     .compileComponents();
@@ -19,10 +25,39 @@ describe('ComentarioDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ComentarioDetailComponent);
     component = fixture.componentInstance;
+
+
+
+    component.comentarioDetail = new ComentarioDetail(
+      faker.datatype.number(),
+      faker.internet.userName(),
+      faker.datatype.string(),
+      faker.datatype.number({min: 1, max: 5}),
+      faker.datatype.string()
+     
+      
+
+    
+
+    
+   
+      
+    );
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a p.h3.p-3 element with comentarioDetail.nombre', () => {
+    const nameElement: HTMLElement = debug.query(By.css('p.h3.p-3')).nativeElement;
+    expect(nameElement.textContent).toContain(component.comentarioDetail.nombre);
+  });
+
+ 
+
+
 });
